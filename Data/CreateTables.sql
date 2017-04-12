aeb5de1d14b17398f0116a6bc9b575e49eb5b36a
baconbum <benaldebert@gmail.com>
2017-04-12 02:07:30 -0400

    Created Initial Database Structure
    Created a simple database structure to contain data about Teams, Franchises, Divisions and Conferences. This will allow me to cache the data with one call to the API instead of making repeated calls, which will greatly increase the speed (last commit saw a drastic slow down). Still need to add more tables, but this is a start.

---
+++ Data/CreateTables.sql
@@ -0,0 +1,42 @@
+BEGIN;
+
+CREATE TABLE Conferences(
+	ID						INTEGER	PRIMARY KEY	NOT NULL,
+	Name					TEXT,
+	ShortName			TEXT,
+	Abbreviation	TEXT,
+	Active				BOOLEAN
+);
+
+CREATE TABLE Divisions(
+	ID						INTEGER	PRIMARY KEY	NOT NULL,
+	ConferenceID	INTEGER,
+	Name					TEXT,
+	Abbreviation	TEXT,
+	Active				BOOLEAN,
+	FOREIGN KEY(ConferenceID) REFERENCES Conferences(ID)
+);
+
+CREATE TABLE Franchises(
+	ID						INTEGER	PRIMARY KEY	NOT NULL,
+	LocationName	TEXT,
+	TeamName			TEXT
+);
+
+CREATE TABLE Teams(
+	ID						INTEGER	PRIMARY KEY	NOT NULL,
+	FranchiseID		INTEGER,
+	DivisionID		INTEGER,
+	ConferenceID	INTEGER,
+	Name					TEXT,
+	LocationName	TEXT,
+	TeamName			TEXT,
+	ShortName			TEXT,
+	Abbreviation	TEXT,
+	Active				BOOLEAN,
+	FOREIGN KEY(FranchiseID)	REFERENCES Franchises(ID),
+	FOREIGN KEY(DivisionID)		REFERENCES Divisions(ID),
+	FOREIGN KEY(ConferenceID)	REFERENCES Conferences(ID)
+);
+
+COMMIT;

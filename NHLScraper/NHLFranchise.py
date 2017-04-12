e847a8a52ffde7e79db2eb291bb2c85926fa9020
baconbum <benaldebert@gmail.com>
2017-04-12 02:11:19 -0400

    Created class to handle Franchises
    Created a class to work with Franchise data. This class can construct the object from JSON data, an ID to be passed to the database, or an ID to be passed to the API. This was written very quickly, I will be reviewing it before creating other classes like it.

---
+++ NHLScraper/NHLFranchise.py
@@ -0,0 +1,77 @@
+#!/usr/bin/env python3
+
+import requests
+import sqlite3
+
+class NHLFranchise:
+	'Details of an NHL Franchise'
+
+	def __init__(self, franchiseData=None, idForDatabase=None, idForAPI=None):
+		if (franchiseData != None):
+			self.__constructFranchiseFromJSON(franchiseData)
+		elif (idForDatabase != None):
+			self.__constructFranchiseFromDatabase(idForDatabase)
+		elif (idForAPI != None):
+			self.__constructFranchiseFromAPI(idForAPI)
+
+	def __constructFranchiseFromJSON(self, franchiseData):
+		self.id =			franchiseData["franchiseId"]
+		self.locationName =	franchiseData["locationName"]
+		self.teamName =		franchiseData["teamName"]
+
+	def __constructFranchiseFromDatabase(self, id):
+		connection =	sqlite3.connect("SportsTicker.db")
+		cursor =		connection.cursor()
+
+		cursor.execute("SELECT ID, LocationName, TeamName FROM Franchises WHERE ID=?", (id,))
+
+		row = cursor.fetchone()
+
+		if (row != None):
+			self.id =			row[0]
+			self.locationName =	row[1]
+			self.teamName =		row[2]
+
+		connection.close()
+
+	def __constructFranchiseFromAPI(self, id):
+		franchise =  NHLFranchise.GetFranchisesFromAPI(idFilter=id)[0]
+
+		self.id =			franchise.id
+		self.locationName =	franchise.locationName
+		self.teamName =		franchise.teamName
+
+	def getFranchisesFromAPI(idFilter=None):
+		franchisesData = NHLFranchise.__getFranchisesDataFromAPI(idFilter)
+
+		franchises = list()
+
+		for franchiseData in franchisesData:
+			franchises.append(NHLFranchise(franchiseData=franchiseData))
+
+		return franchises
+
+	def __getFranchisesDataFromAPI(idFilter=None):
+		franchisesUrl = "https://statsapi.web.nhl.com/api/v1/franchises"
+
+		if (idFilter != None):
+			franchisesUrl += "/{0}".format(idFilter)
+
+		response = requests.get(franchisesUrl)
+		data = response.json()
+
+		return data["franchises"]
+
+	def populateDatabaseWithFranchisesFromAPI():
+		connection =	sqlite3.connect("SportsTicker.db")
+		cursor =		connection.cursor()
+
+		cursor.execute("DELETE FROM Franchises")
+
+		franchises = NHLFranchise.getFranchisesFromAPI()
+
+		for franchise in franchises:
+			cursor.execute("INSERT INTO Franchises (ID, TeamName, LocationName) VALUES (?, ?, ?)", (franchise.id, franchise.teamName, franchise.locationName))
+
+		connection.commit()
+		connection.close()

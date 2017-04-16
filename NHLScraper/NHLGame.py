dc946fa6e2a8a6f96f1be11895266d515fba68b5
baconbum <benaldebert@gmail.com>
2017-04-16 14:53:33 -0400

    Moved display logic to NHLGame
    The display logic was bugging me in the main.py file, I've moved it to NHLGame. Still not quite perfect, but a good start, not bugging me anymore.

--- NHLScraper/NHLGame.py
+++ NHLScraper/NHLGame.py
@@ -8,10 +8,10 @@ class NHLGame:
 	'The details of a particular game'
 
 	def __init__(self, gameData):
-		self.homeTeam = NHLTeam(teamData=gameData["teams"]["home"]["team"])
-		self.awayTeam = NHLTeam(teamData=gameData["teams"]["away"]["team"])
-		self.startTime = pytz.UTC.localize(datetime.datetime.strptime(gameData["gameDate"], "%Y-%m-%dT%H:%M:%SZ"))
-		self.scoringPlays = self.getScoringPlays(gameData["scoringPlays"])
+		self.homeTeam =		NHLTeam(teamData=gameData["teams"]["home"]["team"])
+		self.awayTeam =		NHLTeam(teamData=gameData["teams"]["away"]["team"])
+		self.startTime =	pytz.UTC.localize(datetime.datetime.strptime(gameData["gameDate"], "%Y-%m-%dT%H:%M:%SZ"))
+		self.scoringPlays =	self.getScoringPlays(gameData["scoringPlays"])
 
 	def getScoringPlays(self, scoringPlaysData):
 		scoringPlays = list()
@@ -20,3 +20,26 @@ class NHLGame:
 			scoringPlays.append(NHLScoringPlay(scoringPlayData))
 
 		return scoringPlays
+
+	def getScoringPlayOutput(self, scoringPlayIndex):
+		if (scoringPlayIndex < 0 or scoringPlayIndex > len(self.scoringPlays) - 1):
+			print("No such index exists in NHLGame.scoringPlays. Error occurred in NHLGame.getScoringPlayOutput")
+			return
+
+		scoringPlayOutputLines = list()
+
+		scoringPlay = self.scoringPlays[scoringPlayIndex]
+
+		scoringPlayOutputLines.append("{0} Goal ({1} {2})".format(scoringPlay.team.abbreviation, scoringPlay.periodTime, scoringPlay.periodOrdinal))
+		scoringPlayOutputLines.append("{0} {1} @ {2} {3}".format(self.awayTeam.abbreviation, scoringPlay.awayScore, self.homeTeam.abbreviation, scoringPlay.homeScore))
+		scoringPlayOutputLines.append("{0} ({1})".format(scoringPlay.scorer.fullName, scoringPlay.scorer.seasonTotal))
+		if (len(scoringPlay.assistingPlayers) > 0):
+			assistOutput = "Assisted by: {0} ({1})".format(scoringPlay.assistingPlayers[0].fullName, scoringPlay.assistingPlayers[0].seasonTotal)
+			if (len(scoringPlay.assistingPlayers) > 1):
+				assistOutput += ", {0} ({1})".format(scoringPlay.assistingPlayers[1].fullName, scoringPlay.assistingPlayers[1].seasonTotal)
+
+			scoringPlayOutputLines.append(assistOutput)
+		else:
+			scoringPlayOutputLines.append("Unassisted")
+
+		return scoringPlayOutputLines
--- main.py
+++ main.py
@@ -24,27 +24,17 @@ LCD_BACKLIGHT = 22
 
 localTimeZone = pytz.timezone("Canada/Eastern")
 
-dailySchedule = NHLDailySchedule(datetime.datetime.now(datetime.timezone.utc).astimezone(localTimeZone).date())
-#dailySchedule = NHLDailySchedule(datetime.date(2017, 4, 9))
+#dailySchedule = NHLDailySchedule(datetime.datetime.now(datetime.timezone.utc).astimezone(localTimeZone).date())
+dailySchedule = NHLDailySchedule(datetime.date(2017, 4, 15))
 
 celebration = GoalCelebration(ledPinNumbers=[LED_ONE, LED_TWO, LED_THREE, LED_FOUR], ledPattern=GoalCelebration.AWESOME_PATTERN,
 				lcdPinRS=LCD_RS, lcdPinRW=LCD_RW, lcdPinE=LCD_E, lcdPinData=[LCD_DATA_ONE, LCD_DATA_TWO, LCD_DATA_THREE, LCD_DATA_FOUR], lcdPinBacklight=LCD_BACKLIGHT)
 
 for game in dailySchedule.games:
-	for scoringPlay in game.scoringPlays:
-		lineOne = "{0} Goal ({1} {2})".format(scoringPlay.team.abbreviation, scoringPlay.periodTime, scoringPlay.periodOrdinal)
-		lineTwo = "{0} {1} @ {2} {3}".format(game.awayTeam.abbreviation, scoringPlay.awayScore, game.homeTeam.abbreviation, scoringPlay.homeScore)
-
-		celebration.playCelebration(lineOne=lineOne, lineTwo=lineTwo, ledPatternRepeat=1)
-
-		lineOne = "{0} ({1})".format(scoringPlay.scorer.fullName, scoringPlay.scorer.seasonTotal)
-		if (len(scoringPlay.assistingPlayers) > 0):
-			lineTwo = "Assisted by: {0} ({1})".format(scoringPlay.assistingPlayers[0].fullName, scoringPlay.assistingPlayers[0].seasonTotal)
-			if (len(scoringPlay.assistingPlayers) > 1):
-				lineTwo += ", {0} ({1})".format(scoringPlay.assistingPlayers[1].fullName, scoringPlay.assistingPlayers[1].seasonTotal)
-		else:
-			lineTwo = "Unassisted"
-
-		celebration.playCelebration(lineOne=lineOne, lineTwo=lineTwo, ledPatternRepeat=0)
+	for index, scoringPlay in enumerate(game.scoringPlays):
+		scoringPlayOutput = game.getScoringPlayOutput(index)
+		
+		celebration.playCelebration(lineOne=scoringPlayOutput[0], lineTwo=scoringPlayOutput[1], ledPatternRepeat=1)
+		celebration.playCelebration(lineOne=scoringPlayOutput[2], lineTwo=scoringPlayOutput[3], ledPatternRepeat=0)
 
 GPIO.cleanup()

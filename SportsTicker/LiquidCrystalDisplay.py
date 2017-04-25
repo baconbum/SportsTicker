19db292b5ba9b5a36272af13d5897666067abba8
baconbum <benaldebert@gmail.com>
2017-04-24 22:10:53 -0400

    Improved LCD class
    The LiquidCrystalDisplay class now properly inherits the CharLCD class instead of having a CharLCD object as a property. Also, the LCD height/width are now set in the config file rather than hard coded in the class itself.

--- SportsTicker/LiquidCrystalDisplay.py
+++ SportsTicker/LiquidCrystalDisplay.py
@@ -1,36 +1,41 @@
 #!/usr/bin/env python3
 
 import time
+from configparser import ConfigParser
 import RPi.GPIO as GPIO
 from RPLCD import CharLCD, BacklightMode
 
-class LiquidCrystalDisplay:
-	'Class for a LCD 1602'
-
-	LCD_WIDTH =		16
-	LCD_HEIGHT =	2
+class LiquidCrystalDisplay(CharLCD):
+	'Class for a LCD.'
 
 	def __init__(self, pin_rs, pin_rw, pin_e, pins_data, pin_backlight=None, backlight_enabled=False):
 
-		self.lcd = CharLCD(pin_rs=pin_rs, pin_rw=pin_rw, pin_e=pin_e, pins_data=pins_data, pin_backlight=pin_backlight,
-			numbering_mode=GPIO.BOARD,
-			cols=self.LCD_WIDTH, rows=self.LCD_HEIGHT, dotsize=8,
-			auto_linebreaks=True,
-			backlight_enabled=backlight_enabled,
-			backlight_mode=BacklightMode.active_high)
+		config = ConfigParser(allow_no_value=True)
+		config.read('config.ini')
+
+		super(LiquidCrystalDisplay, self).__init__(
+			pin_rs=				pin_rs,
+			pin_rw=				pin_rw,
+			pin_e=				pin_e,
+			pins_data=			pins_data,
+			pin_backlight=		pin_backlight,
+			cols=				int(config.get('lcdSize', 'LCD_WIDTH')),
+			rows=				int(config.get('lcdSize', 'LCD_HEIGHT')),
+			backlight_enabled=	backlight_enabled,
+			backlight_mode=		BacklightMode.active_high)
 
 	def toggleBacklight(self, state=None):
 		if (state == None):
-			state = not self.lcd.backlight_enabled
+			state = not self.backlight_enabled
 
 		if (state):
 			print("Turning backlight on")
 		else:
 			print("Turning backlight off")
 
-		self.lcd.backlight_enabled = state
+		self.backlight_enabled = state
 
-		if (self.lcd.backlight_enabled):
+		if (self.backlight_enabled):
 			print("Backlight turned on")
 		else:
 			print("Backlight turned off")
@@ -40,7 +45,7 @@ class LiquidCrystalDisplay:
 			print ("Warning (LiquidCrystalDisplay.displayText): Text parameter is empty")
 			return
 
-		maxCharacters = self.LCD_WIDTH * self.LCD_HEIGHT
+		maxCharacters = self.lcd.cols * self.lcd.rows
 
 		self.toggleBacklight(True)
 
@@ -50,9 +55,9 @@ class LiquidCrystalDisplay:
 			print ("Trimming " + text + " to " + text[:maxCharacters])
 			text = text[:maxCharacters]
 
-		self.lcd.write_string(text)
+		self.write_string(text)
 		time.sleep(duration)
-		self.lcd.clear()
+		self.clear()
 
 		self.toggleBacklight(False)
 
@@ -64,15 +69,15 @@ class LiquidCrystalDisplay:
 		self.toggleBacklight(True)
 
 		# Determine how many ticks it will take to dislay lineOne
-		lineOneTicks = len(lineOne) - self.LCD_WIDTH + 1 if len(lineOne) > 16 else 1
+		lineOneTicks = len(lineOne) - self.lcd.cols + 1 if len(lineOne) > 16 else 1
 
 		# Scroll through lineOne, while keeping a maximum of 16 characters of lineTwo stationary (if it's not None)
 		for i in range(0, lineOneTicks):
-			self.lcd.clear()
-			self.writeTextToRow(lineOne[i:i + self.LCD_WIDTH], 0)
+			self.clear()
+			self.writeTextToRow(lineOne[i:i + self.lcd.cols], 0)
 
 			if (lineTwo != None):
-				self.writeTextToRow(lineTwo[0:self.LCD_WIDTH], 1)
+				self.writeTextToRow(lineTwo[0:self.lcd.cols], 1)
 
 			time.sleep(lineOneDelay if i == 0 else tick)
 
@@ -80,31 +85,31 @@ class LiquidCrystalDisplay:
 			time.sleep(lineOneResetDelay)
 
 			# Determine how many ticks it will take to dislay lineTwo
-			lineTwoTicks = len(lineTwo) - self.LCD_WIDTH + 1 if len(lineTwo) > 16 else 1
+			lineTwoTicks = len(lineTwo) - self.lcd.cols + 1 if len(lineTwo) > 16 else 1
 
 			# Scroll through lineTwo, while keeping a maximum of 16 characters of lineOne stationary
 			for i in range(0, lineTwoTicks):
-				self.lcd.clear()
-				self.writeTextToRow(lineOne[0:self.LCD_WIDTH],		0)
-				self.writeTextToRow(lineTwo[i:i + self.LCD_WIDTH],	1)
+				self.clear()
+				self.writeTextToRow(lineOne[0:self.lcd.cols],		0)
+				self.writeTextToRow(lineTwo[i:i + self.lcd.cols],	1)
 
 				time.sleep(lineTwoDelay if i == 0 else tick)
 
 		time.sleep(clearDelay)
-		self.lcd.clear()
+		self.clear()
 
 		self.toggleBacklight(False)
 
 	def writeTextToRow(self, text, row):
-		if (row > self.LCD_HEIGHT - 1):
+		if (row > self.lcd.rows - 1):
 			print ("Warning (LiquidCrystalDisplay.writeTextToRow): Row parameter is too high")
 			return
 
 		print("Writing " + text + " to LCD row " + str(row))
 
-		if (len(text) > self.LCD_WIDTH):
-			print ("Trimming " + text + " to " + text[:self.LCD_WIDTH])
-			text = text[:self.LCD_WIDTH]
+		if (len(text) > self.lcd.cols):
+			print ("Trimming " + text + " to " + text[:self.lcd.cols])
+			text = text[:self.lcd.cols]
 
-		self.lcd.cursor_pos = (row, 0)
-		self.lcd.write_string(text)
+		self.cursor_pos = (row, 0)
+		self.write_string(text)

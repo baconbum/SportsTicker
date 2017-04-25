e36f1b58a1b18cd7d7b90ff7fe443f39dbd8eeef
baconbum <benaldebert@gmail.com>
2017-04-24 20:53:43 -0400

    Created database initialization script
    Created a script that can be run to (re)populate the database with data from the API. Includes a script to drop all tables.

---
+++ initializeDatabases.py
@@ -0,0 +1,31 @@
+import sqlite3
+
+from NHLScraper.NHLConference import NHLConference
+from NHLScraper.NHLDivision import NHLDivision
+from NHLScraper.NHLFranchise import NHLFranchise
+from NHLScraper.NHLTeam import NHLTeam
+
+def initializeDatabases():
+
+	connection =	sqlite3.connect("SportsTicker.db")
+	cursor =		connection.cursor()
+
+	# Drop all tables (if they exist)
+	tableDeletionSQLPath = open('Data/DropTables.sql','r')
+	tableDeletionSQL = tableDeletionSQLPath.read()
+	connection.executescript(tableDeletionSQL)
+
+	# Create all tables
+	tableCreationSQLPath = open('Data/CreateTables.sql','r')
+	tableCreationSQL = tableCreationSQLPath.read()
+	connection.executescript(tableCreationSQL)
+
+	connection.close()
+
+	# Populate tables with data from API
+	NHLConference.populateConferencesTableFromAPI()
+	NHLDivision.populateDivisionsTableFromAPI()
+	NHLFranchise.populateFranchisesTableFromAPI()
+	NHLTeam.populateTeamsTableFromAPI()
+
+initializeDatabases()
